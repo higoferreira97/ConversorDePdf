c43255a186770081bb6872432bc7c9bce20679f9
higoferreira97 <higoferreira.dev@gmail.com>
2025-02-25 21:08:29 -0300

    Adicionar arquivos de projeto.

---
+++ ConversorDePdf.API/Controllers/ConversorPdfController.cs
@@ -0,0 +1,21 @@
+﻿using ConversorDePdf.API.Controllers.Helpers;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ConversorDePdf.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ConversorPdfController : Controller
+    {
+        [HttpGet]
+        public async Task<ActionResult> converterArquivo()
+        {
+            var basePath = Directory.GetCurrentDirectory();
+            var arquivo = Path.Combine(basePath, "uploads", "teste.docx");
+
+            var arquivoPdf = PdfHelper.convertToPdf(arquivo);
+
+            return File(arquivoPdf, "application/pdf", "teste.pdf");
+        }
+    }
+}
